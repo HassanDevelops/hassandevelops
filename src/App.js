096f5b4ab6ca598d45a3b8bbcc3a603103730360
HassanDevelops <59549481+HassanDevelops@users.noreply.github.com>
2020-06-03 14:29:37 +0500

    grid

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import ParagrapghGrey from "./components/ParagrapghGrey";
 const App = () => {
   return (
     <>
-      <SecondaryHeading headingText="Hi, my name is" />
-      <TitleHeading headingText="Ali Hassan. " />
-      <ThirdHeading headingText="I build things for the web." />
-      <ParagrapghGrey paragrapghText="I'm an engineer based in Lahore, MA specializing in building (and occasionally designing) exceptional websites, applications, and everything in between." />
-      <SecondaryHeading headingText="hello second heading" />
+      <div className="container">
+        <div className="container-child">
+          <SecondaryHeading headingText="Hi, my name is" />
+          <TitleHeading headingText="Ali Hassan. " />
+          <ThirdHeading headingText="I build things for the web." />
+          <ParagrapghGrey paragrapghText="I'm an engineer based in Lahore, MA specializing in building (and occasionally designing) exceptional websites, applications, and everything in between." />
+        </div>
+      </div>
     </>
   );
 };
