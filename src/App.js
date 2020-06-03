323d3f38f2bce05ab5b16666ada3dfebf7031321
HassanDevelops <59549481+HassanDevelops@users.noreply.github.com>
2020-06-03 12:42:58 +0500

    paragrapgh is added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./styleSheets/App.css";
 import TitleHeading from "./components/TitleHeading";
 import SecondaryHeading from "./components/SecondaryHeading";
 import ThirdHeading from "./components/ThirdHeading";
+import ParagrapghGrey from "./components/ParagrapghGrey";
 
 const App = () => {
   return (
     <>
-      <div>hello world.!</div>
       <SecondaryHeading headingText="Hi, my name is" />
       <TitleHeading headingText="Ali Hassan. " />
       <ThirdHeading headingText="I build things for the web." />
+      <ParagrapghGrey paragrapghText="I'm an engineer based in Lahore, MA specializing in building (and occasionally designing) exceptional websites, applications, and everything in between." />
       <SecondaryHeading headingText="hello second heading" />
     </>
   );
---
+++ src/components/ParagrapghGrey.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ParagrapghGrey = ({ paragrapghText }) => {
+  return <div className="paragrapgh-grey">{paragrapghText}</div>;
+};
+
+export default ParagrapghGrey;
